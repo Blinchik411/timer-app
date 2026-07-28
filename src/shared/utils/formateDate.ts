b0c173f2b09a508a4d11fd0496a1d5db3c572ab4
Blin__ <okunevmaksim2502@gmail.com>
2026-07-28 18:51:32 +0300

    Formatted Date in YearlyChart.tsx

---
+++ src/shared/utils/formateDate.ts
@@ -0,0 +1,7 @@
+export const formatDate = (dateStr: string): string => {
+    if (!dateStr) return '';
+
+    const [year, month, day] = dateStr.split('-');
+
+    return `${day}.${month}.${year}`;
+};
